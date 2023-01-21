b1f7be059b1898f8b0b09998836ea354482e1701
Prabhu Subramanian <prabhu@ngcloud.io>
2023-01-21 13:18:41 -0800

    Github actions

--- build.ps1
+++
@@ -1,5 +0,0 @@
-# poetry install
-rustup target add x86_64-pc-windows-msvc
-poetry export -f requirements.txt > requirements-dist.txt
-pyoxidizer build --release --target-triple x86_64-pc-windows-msvc
-rm requirements-dist.txt
