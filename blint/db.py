be16b65f53eb416f7fba698c29fc4ed3ea2b0780
Aryan Rajoria <aryanrajoria1003@gmail.com>
2024-12-18 16:39:54 +0100

    code cleanup
    Signed-off-by: Aryan Rajoria <aryanrajoria1003@gmail.com>

--- blint/db.py
+++ blint/db.py
@@ -12,18 +12,6 @@ from blint.logger import LOG
 
 DEBUG_MODE = os.getenv("SCAN_DEBUG_MODE") == "debug"
 
-# def get_bnames_ename(symbols_name):
-#     """
-#     Older algorithm with many false positives
-#     """
-#     bin_name = []
-#     eid = get_export_id(symbols_name)
-#     bid_list = get_bid_using_fid(eid)
-#     if bid_list:
-#         bin_name.extend(get_bname(bid) for bid in bid_list)
-#     return bin_name
-
-
 def return_batch_binaries_detected(symbols_list):
     """
     Current scoring algorithm along with batching
@@ -98,41 +86,8 @@ def get_bname(bid):
     return res[0][0] if res else None
 
 
-# def detect_binaries_utilized(sybmols_list) -> set:
-#     """ Simple Voting algorithm
-#     for a given symbols. e.g. XRenderAddGlyphs
-#     we count the number of binaries associated to this function
-#     e.g. which is one in this example XRenderAddGlyphs
-#     is associated with 'libXrender-0.9.10/libxrender.so'.
-#     so one is added to score, we want all the detections to have a score greater than 1.
-#     """
-#     bin_detected_dict = {}
-
-#     # eid_list = [get_export_id(symbol['name']) for symbol in sybmols_list]
-#     eid_list = [symbol["name"] for symbol in sybmols_list]
-#     # creates a 2D array with batch_len, batch_len eids are processed in a single query
-#     batch_len = 1000
-#     eid_2d_list = [eid_list[i : i + batch_len] for i in range(0, len(eid_list), batch_len)]
-
-#     LOG.debug(f"Created {len(eid_2d_list)} processes created")
-#     # for eid in eid_list:
-#     with concurrent.futures.ProcessPoolExecutor(max_workers=13) as executor:
-#         for _, single_binaries_detected_dict in zip(
-#             eid_2d_list, executor.map(return_batch_binaries_detected, eid_2d_list)
-#         ):
-#             for fname, score in single_binaries_detected_dict.items():
-#                 if fname in bin_detected_dict:
-#                     bin_detected_dict[fname] += score
-#                 else:
-#                     bin_detected_dict[fname] = score
-#     # create a set() and remove false positives
-#     binary_detected = {bname for bname, score in bin_detected_dict.items() if score > 1}
-
-#     LOG.debug(f"Output for binary_detected: {binary_detected}")
-#     return binary_detected
-
 def detect_binaries_utilized(sybmols_list) -> set:
-    """ Simple Voting algorithm
+    """Simple Voting algorithm
     for a given symbols. e.g. XRenderAddGlyphs
     we count the number of binaries associated to this function
     e.g. which is one in this example XRenderAddGlyphs
@@ -141,7 +96,6 @@ def detect_binaries_utilized(sybmols_list) -> set:
     """
     bin_detected_dict = {}
 
-    # eid_list = [get_export_id(symbol['name']) for symbol in sybmols_list]
     eid_list = [symbol["name"] for symbol in sybmols_list]
     # creates a 2D array with batch_len, batch_len eids are processed in a single query
     batch_len = 1000
@@ -151,9 +105,9 @@ def detect_binaries_utilized(sybmols_list) -> set:
     # for eid in eid_list:
     with concurrent.futures.ProcessPoolExecutor() as executor:
         futures_bin_detected = {
-            executor.submit(return_batch_binaries_detected, it_eid_list): it_eid_list 
+            executor.submit(return_batch_binaries_detected, it_eid_list): it_eid_list
             for it_eid_list in eid_2d_list
-            }
+        }
         for future in concurrent.futures.as_completed(futures_bin_detected):
             single_binaries_detected_dict = future.result()
             for fname, score in single_binaries_detected_dict.items():
@@ -166,78 +120,6 @@ def detect_binaries_utilized(sybmols_list) -> set:
 
     LOG.debug(f"Output for binary_detected: {len(binary_detected)}")
     return binary_detected
-
-def return_binaries_detected(eid):
-    """
-    Current scoring algorithm. Unbatched
-    """
-    binaries_detected_dict = {}
-    bid_list = get_bid_using_fid(eid)
-    if not bid_list:
-        return {}
-    bid_list = list(bid_list)
-    score = 1 / len(bid_list)
-    for bid in bid_list:
-        bname = get_bname(bid)
-        if bname in binaries_detected_dict:
-            binaries_detected_dict[bname] += score
-        else:
-            binaries_detected_dict[bname] = score
-    return binaries_detected_dict
-
-
-# Non batching
-def detect_binaries_utilized_unbatched(sybmols_list) -> set:
-    """ Simple Voting algorithm
-    for a given symbols. e.g. XRenderAddGlyphs
-    we count the number of binaries associated to this function
-    e.g. which is one in this example XRenderAddGlyphs
-    is associated with 'libXrender-0.9.10/libxrender.so'.
-    so one is added to score, we want all the detections to have a score greater than 1.
-    """
-    bin_detected_dict = {}
-    eid_list = [get_export_id(symbol["name"]) for symbol in sybmols_list]
-    # for eid in eid_list:
-    with concurrent.futures.ProcessPoolExecutor(max_workers=13) as executor:
-        for _, single_binaries_detected_dict in zip(
-            eid_list, executor.map(return_binaries_detected, eid_list)
-        ):
-            for fname, score in single_binaries_detected_dict.items():
-                if fname in bin_detected_dict:
-                    bin_detected_dict[fname] += score
-                else:
-                    bin_detected_dict[fname] = score
-    # create a set() and remove false positives
-    binary_detected = {bname for bname, score in bin_detected_dict.items() if score > 1}
-    return binary_detected
-
-
-def get_bid_using_eid_batch(batch_eid_list):
-    """Retrieves binary IDs for a batch of export IDs from a SQLite database. The function performs a grouped query to fetch associated binary identifiers.
-
-    This function connects to a SQLite database using the BLINTDB_LOC environment variable and executes a query to retrieve binary IDs for a given list of export IDs. It supports batch processing, allowing efficient lookup of related binary identifiers.
-
-    Args:
-        batch_eid_list (list): A list of export IDs to search for in the database.
-
-    Returns:
-        list or None: A list of binary IDs if matching export IDs are found, otherwise None.
-
-    Raises:
-        sqlite3.Error: If there are any database connection or query execution issues.
-    """
-    BLINTDB_LOC = os.getenv("BLINTDB_LOC")
-    with closing(sqlite3.connect(BLINTDB_LOC)) as connection:
-        with closing(connection.cursor()) as c:
-            place_holders = "?, " * (len(batch_eid_list) - 1) + "?"
-            if len(batch_eid_list) > 0:
-                output_string = f"SELECT eid, group_concat(bid) from BinariesExports where eid IN ({place_holders}) group by eid"
-                print(output_string)
-                c.execute(output_string, (batch_eid_list,))
-            res = c.fetchall()
-        connection.commit()
-    return map(lambda x: x[0], res) if res else None
-
 
 def get_export_id(export_name):
     """Retrieves the export ID for a given export name from a SQLite database. The function performs a lookup to fetch the corresponding export identifier.
