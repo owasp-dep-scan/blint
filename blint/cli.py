29d3551c6a0737673ef12ab91583d40c50a2ce8d
prabhu <7842+prabhu@users.noreply.github.com>
2025-03-24 02:38:02 +0000

    Adds vcpkg arm64 image option
    Signed-off-by: prabhu <7842+prabhu@users.noreply.github.com>

--- blint/cli.py
+++ blint/cli.py
@@ -177,7 +177,8 @@ def build_parser():
     db_parser.add_argument(
         "--image-url",
         dest="image_url",
-        choices=["ghcr.io/appthreat/blintdb-vcpkg-darwin-arm64:v1",
+        choices=["ghcr.io/appthreat/blintdb-vcpkg-arm64:v1",
+                 "ghcr.io/appthreat/blintdb-vcpkg-darwin-arm64:v1",
                  "ghcr.io/appthreat/blintdb-meson-darwin-arm64:v1",
                  "ghcr.io/appthreat/blintdb-meson:v1",
                  "ghcr.io/appthreat/blintdb-meson-arm64:v1"
